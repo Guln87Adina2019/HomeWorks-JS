52d91456d984aba73e05087c59e594f72bf16d34
Gulnara <adinadzulia@gmail.com>
2024-11-27 08:56:44 +0500

    arrayaaaaaaaaaaaaa

---
+++ hw3-week12-JS/hw3week12-js/hw4-week12-js/array.js
@@ -0,0 +1,21 @@
+let player = ["Рони","Зидан","Ануар","Бензема"]
+
+console.log(player);
+
+let player1 = ["Рони","Зидан","Неймар","Бензема"]
+player1 [1] = 'Месси'
+console.log(player1);
+
+let player2 = ["Рони","Зидан","Неймар","Бензема"]
+player2 [4] = 'Месси'
+player2 [5] = 'Пеле'
+console.log(player2);
+
+
+let fplayer =+prompt("фудболистер саны")
+if (fplayer > 5){
+    alert("коп")
+}
+else if(fplayer < 5 ){
+alert("аз")
+}
