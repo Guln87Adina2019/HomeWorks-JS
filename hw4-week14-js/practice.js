9fe197ad9f7fe7965eb2ed69fb40ac6b35b312d4
Gulnara <adinadzulia@gmail.com>
2024-12-07 00:05:17 +0500

    добавили изменении  hw4-week14-js

--- hw4-week14-js/practice.js
+++ hw4-week14-js/practice.js
@@ -35,15 +35,49 @@
 
 2///////////
 
-let product = [
-    {name:"Samsung",price:300000,},
-    {name:"Iphone",price:400000,},
-    {name:"Meizu",price:180000,},
-    {name:"Huawei",price:200000,}
-]
-console.log(product);
-////////////////////////////
-let evaluations2 = product.filter(product=>product.price>200000)
-console.log(evaluations2);
-//////////////////////////
-console.log(evaluations2.reduce((total, num)=>total + num,0));
+// let product = [
+//     {name:"Samsung",price:300000,},
+//     {name:"Iphone",price:400000,},
+//     {name:"Meizu",price:180000,},
+//     {name:"Huawei",price:200000,}
+// ]
+// console.log(product);
+// ////////////////////////////
+// let evaluations2 = product.filter(product=>product.price>200000)
+// console.log(evaluations2);
+// //////////////////////////
+// console.log(evaluations2.reduce((total, num)=>total + num,0));
+
+
+// Күнделікті тапсырма массиві (атау және орындалған/орындалмаған дерегі)
+let tasks = [
+    { name: "Тапсырма 1", completed: false },
+    { name: "Тапсырма 2", completed: true },
+    { name: "Тапсырма 3", completed: false },
+    { name: "Тапсырма 4", completed: true },
+    { name: "Тапсырма 5", completed: false }
+  ];
+  
+  // Орындалмаған тапсырмаларды анықтау және шығару
+  let incompleteTasks = tasks.filter(task => !task.completed);
+  if (incompleteTasks.length > 0) {
+    alert("Орындалмаған тапсырмалар:");
+    incompleteTasks.forEach(task => alert(task.name));
+  } else {
+    alert("Барлық тапсырмалар орындалған!");
+  }
+  
+  // Белгілі бір тапсырманы аяқталған деп белгілеу
+  let taskNumber = prompt("Аяқталған деп белгілеу үшін тапсырма нөмірін енгізіңіз:");
+  taskNumber = parseInt(taskNumber) - 1; // Пайдаланушы 1-ден бастайды деп болжасақ
+  if (taskNumber >= 0 && taskNumber < tasks.length) {
+    tasks[taskNumber].completed = true;
+    alert(${tasks[taskNumber].name} аяқталды!);
+  } else {
+    alert("Жарамсыз нөмір енгізілді.");
+  }
+  
+  // Тек аяқталған тапсырмаларды шығару
+  let completedTasks = tasks.filter(task => task.completed);
+  console.log("Аяқталған тапсырмалар:");
+  completedTasks.forEach(task => console.log(task.name));
